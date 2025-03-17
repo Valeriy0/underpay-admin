1ce48faf1771dbbe5a3dda4283a33c705a57ddfa
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-18 00:44:33 +0300

    update

--- src/RoutesFind.js
+++ src/RoutesFind.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Banners } from './pages/banners';
 import { ProductsPage } from './pages/products';
 import { CategoriesPage } from './pages/categories';
+import { AuthPage } from './pages/auth';
 
 export const RoutesFind = () => {
   return (
     <Router>
       <Routes>
+        <Route path='/auth' element={<AuthPage />} />
         <Route path='/banners' element={<Banners />} />
         <Route path='/categories' element={<CategoriesPage />} />
         <Route path='/products' element={<ProductsPage />} />
---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import { LoginButton } from "@telegram-auth/react";
+
+export const AuthPage = () => {
+    return (
+        <div className="flex items-center justify-center">
+             <LoginButton
+                botUsername={'payTestUnder_bot'}
+                onAuthCallback={(data) => {
+                    console.log(data);
+                    // call your backend here to validate the data and sign in the user
+                }}
+            />
+        </div>
+    )
+}
