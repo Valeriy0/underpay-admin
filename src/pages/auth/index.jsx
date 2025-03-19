81cefb3ad7028bea48f6d8ee41d8fd4a8d9c2dae
«Valer000n» <valeriy0@csfail.com>
2025-03-19 23:03:39 +0300

    fix

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -4,14 +4,7 @@ import { LoginButton } from "@telegram-auth/react";
 export const AuthPage = () => {
     return (
         <div className="flex items-center justify-center">
-             <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="payTestUnder_bot" data-size="large" data-userpic="false" data-auth-url="/" data-request-access="write"></script>
-             <LoginButton
-                botUsername={'payTestUnder_bot'}
-                onAuthCallback={(data) => {
-                    console.log(data);
-                    // call your backend here to validate the data and sign in the user
-                }}
-            />
+          
         </div>
     )
 }
