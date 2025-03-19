292b735979dc2b7d0b89ee969e735c871c5a4134
«Valer000n» <valeriy0@csfail.com>
2025-03-19 23:48:16 +0300

    fix

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -4,7 +4,13 @@ import { LoginButton } from "@telegram-auth/react";
 export const AuthPage = () => {
     return (
         <div className="flex items-center justify-center">
-          
+             <LoginButton
+                botUsername={'payTestUnder_bot'}
+                onAuthCallback={(data) => {
+                    console.log(data);
+                    // call your backend here to validate the data and sign in the user
+                }}
+            />
         </div>
     )
 }
